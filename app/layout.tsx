49e8d8b9f4116fc5ef93faecfcfc34b18a500e86
Никита Тарба <nicktarba@MacBook-Air.local>
2026-06-14 18:33:10 +1000

    revert home page redesign

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Fraunces:ital,wght@0,700;1,700&family=Playfair+Display:ital,wght@0,600;0,700;0,800;1,600&family=Caveat:wght@500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Fraunces:ital,wght@0,700;1,700&display=swap" rel="stylesheet" />
       </head>
       <body>{children}</body>
     </html>
